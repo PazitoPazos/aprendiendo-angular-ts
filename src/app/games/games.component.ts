c76efda8164e4bb887c441e5f22e93dc9e717be3
PazitoPazos <javier.pazos001@gmail.com>
2023-12-19 19:34:34 +0100

    Added components

---
+++ src/app/games/games.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-games',
+  standalone: true,
+  imports: [],
+  template: `
+    <h3>Los juegos favoritos de {{ username }}</h3>
+    <ul>
+      @for (game of games; track game.id) {
+        <li>{{ game.name }}</li>
+      }
+    </ul>
+  `,
+  styles: ``
+})
+export class GamesComponent {
+  @Input() username = '';
+
+  games = [
+    {
+      id: 1,
+      name: 'Uncharted 4'
+    },
+    {
+      id: 2,
+      name: 'Horizon Zero Dawn'
+    },
+    {
+      id: 3,
+      name: 'Bloodborne'
+    },
+  ]
+}
